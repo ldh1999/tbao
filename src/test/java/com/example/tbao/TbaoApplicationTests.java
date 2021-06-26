41535371822dbd736486d6139ca0069747de5511
13920574532 <13920574532@zrgj.com>
2021-06-26 20:53:58 +0800

    commit first

---
+++ src/test/java/com/example/tbao/TbaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tbao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
